6541060e123d5db5e2123e77c688cb86e5746cd0
Admin <zafaraabid3295@gmail.com>
2019-11-11 16:17:41 +0530

    mathematics Calculation.

--- mathematicsCalculation.sh
+++ mathematicsCalculation.sh
@@ -2,5 +2,10 @@ read -p "Enter first number:"  value1
 read -p "Enter second number:"  value2
 read -p "Enter third number:"  value3
 
+function mathematics() {
 result=$(( $value1 + $value2 * $value3 ))
-echo $result
+echo $result 
+}
+
+result1="$( mathematics ) "
+echo $result1
