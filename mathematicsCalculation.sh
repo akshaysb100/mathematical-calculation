3cfe71077f11e5ab786df8c31c55efe478db2815
Admin <zafaraabid3295@gmail.com>
2019-11-11 16:34:14 +0530

    add branch 1

--- mathematicsCalculation.sh
+++ mathematicsCalculation.sh
@@ -1,6 +1,20 @@
+#!/bin/bash -x 
 read -p "Enter first number:"  value1
 read -p "Enter second number:"  value2
 read -p "Enter third number:"  value3
 
+function mathematics() {
 result=$(( $value1 + $value2 * $value3 ))
-echo $result
+echo $result 
+}
+
+result1="$( mathematics ) "
+echo $result1
+
+function mathematics1() {
+result=$(( $value1 * $value2 + $value3 ))
+echo $result 
+}
+
+result2="$( mathematics1 )"
+echo $result1
